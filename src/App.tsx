72954c33c89561bbe197510a569718f3cc11e35e
umutplvn <umutpehlivan2078@gmail.com>
2025-05-01 22:55:03 +0100

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 
-// import { createTheme, ThemeProvider } from "@mui/material/styles"
 import AppRouter from "./router/AppRouter"
 import { Provider } from "react-redux"
-import store from "./app/store"
-
-// import { ToastContainer } from "react-toastify"
-// import { PersistGate } from "redux-persist/integration/react"
+import store, {persistor} from "./app/store"
+import "./index.css"
+import { PersistGate } from "redux-persist/integration/react"
 
 function App() {
 
 
   return (
     <>
-        <Provider store={store}
-         >
-          {/* <PersistGate loading={null} persistor={persistor}> */}
+        <Provider store={store} >
+          <PersistGate loading={null} persistor={persistor}>
             <AppRouter />
-          {/* </PersistGate> */}
+          </PersistGate>
         </Provider>
-        {/* <ToastContainer /> */}
     </>
   )
 }
