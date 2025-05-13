a626d8958361d6a61073ad090fe92a6a9e00c4cf
umutplvn <umutpehlivan2078@gmail.com>
2025-05-13 05:34:26 +0100

    home page update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import image from '../assets/10041622.png';
 import BlurText from "../components/BlurText";
 import ShinyText from '../components/ShinyText';
 import DecryptedText from '../components/DecryptedText';
-
 import '../styles/homeStyle.css';
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   const handleNavigate = () => {
     navigate('/login');
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setImageLoaded(true);
+    }, 300); 
+  }, []);
 
   return (
     <div
@@ -25,13 +31,15 @@ const Home: React.FC = () => {
         alignItems: 'center',
       }}
     >
-      <div style={{
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}>
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
         <div
           style={{
             backgroundImage: `url(${image})`,
@@ -41,6 +49,8 @@ const Home: React.FC = () => {
             width: '20rem',
             height: '20rem',
             borderRadius: '50%',
+            transition: 'all 1s ease-out',
+            filter: imageLoaded ? 'brightness(1)' : 'brightness(0.1)', 
           }}
           aria-label="Profile Image"
         />
@@ -53,27 +63,25 @@ const Home: React.FC = () => {
         />
       </div>
 
-      <div style={{marginBottom:"2rem", height:"25rem", padding:"2rem 1rem"}}>
-      <DecryptedText
-        text="Upload your resume here to begin the online interview process. This platform provides an efficient way for you to present your qualifications and expertise, enabling you to engage directly with potential employers in a professional setting."
-        speed={80}
-        maxIterations={20}
-        characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+"
-        className="revealed"
-        parentClassName="all-letters"
-        encryptedClassName="encrypted"
-        
-      />
-
+      <div style={{ marginBottom: '2rem', height: '25rem', padding: '2rem 1rem' }}>
+        <DecryptedText
+          text="Upload your resume here to begin the online interview process. This platform provides an efficient way for you to present your qualifications and expertise, enabling you to engage directly with potential employers in a professional setting."
+          speed={80}
+          maxIterations={20}
+          characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+"
+          className="revealed"
+          parentClassName="all-letters"
+          encryptedClassName="encrypted"
+        />
       </div>
 
-      <div style={{ position:"relative", bottom:"5rem"}}>
+      <div style={{ position: 'relative', bottom: '5rem' }}>
         <button
           className="btn"
           style={{
-            backgroundColor: "black",
+            backgroundColor: 'black',
             borderRadius: '1rem',
-            cursor: "pointer",
+            cursor: 'pointer',
           }}
           onClick={handleNavigate}
           aria-label="Go to Login Page"
