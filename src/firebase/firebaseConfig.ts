3bf8cc7f65bb274376a4d32fe7eeb3b3049daae9
umutplvn <umutpehlivan2078@gmail.com>
2025-05-13 12:48:09 +0100

    up

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"
+import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdanyQbj_mEq6PzOSZPMCLnir1bgCZzJE",
-  authDomain: "ai-interview-app-9db16.firebaseapp.com",
-  projectId: "ai-interview-app-9db16",
-  storageBucket: "ai-interview-app-9db16.firebasestorage.app",
-  messagingSenderId: "377560752280",
-  appId: "1:377560752280:web:ee9f045152dbcf6be330ed",
-  measurementId: "G-4P9GWVQMZ5"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY!,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID!,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID!,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
