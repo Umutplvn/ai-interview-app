29750757d106aacb7469c724235b48d05745a960
umutplvn <umutpehlivan2078@gmail.com>
2025-05-14 22:19:20 +0100

    up

--- src/pages/Interview.tsx
+++ src/pages/Interview.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+const apiKey = process.env.REACT_APP_GEMINI_API!;
 
 const Interview = () => {
-  return (
-    <div>Interview</div>
-  )
-}
+  useEffect(() => {
+    if (!apiKey) {
+      console.error("API anahtarı tanımlı değil");
+      return;
+    }
 
-export default Interview
+    const genAI = new GoogleGenerativeAI(apiKey);
+
+    async function main() {
+      try {
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+        const result = await model.generateContent("Explain how AI works in a few words");
+        const response = await result.response;
+        const text = await response.text();
+        console.log(text);
+      } catch (error) {
+        console.error("Gemini hatası:", error);
+      }
+    }
+    main();
+  }, []);
+
+  return <div>Interview Page</div>;
+};
+
+export default Interview;
